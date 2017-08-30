238d5fc563914e79623ae8d8361986872d30d01e
RobertoRomeroHernandez <31378324+RobertoRomeroHernandez@users.noreply.github.com>
2017-08-30 12:29:06 -0500

    Programa python 1

--- temp.py
+++ temp.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 plt.plot([6,7,8,8,9,9,10,11,11,12,12,12,12,12,12,12,12,12,13,13,14])
-plt.ylabel('[Número total de primos y hermanos]')
+plt.ylabel('[Numero total de primos y hermanos]')
 plt.savefig('temp.png')
 plt.show()
 
